600188dc1c3f2637db44f92b0dc301a54ea0d147
henriquejosefaria <a82200@alunos.uminho.pt>
2022-01-17 16:16:35 +0000

    new version of mpc_AND

--- ZKBoo_pieces/mpc_functions/compiled_Jasmin/mpc_AND.s
+++ ZKBoo_pieces/mpc_functions/compiled_Jasmin/mpc_AND.s
@@ -4,26 +4,18 @@
 	.globl	test
 _test:
 test:
-	movq	$0, %rcx
-	movq	$2, %rsi
 	movq	$0, %rax
-	jmp 	Ltest$4
-Ltest$5:
-	incq	%rax
+	jmp 	Ltest$3
 Ltest$4:
+	incq	%rax
+Ltest$3:
 	cmpq	$3, %rax
-	jb  	Ltest$5
-	movq	$0, %rdi
+	jb  	Ltest$4
+	movq	$0, %rax
 	jmp 	Ltest$1
 Ltest$2:
-	movq	%rcx, %rax
-	movq	$0, %rdx
-	divq	%rsi
-	cmpq	$0, %rdx
-	je  	Ltest$3
-Ltest$3:
-	incq	%rdi
+	incq	%rax
 Ltest$1:
-	cmpq	$3, %rdi
+	cmpq	$3, %rax
 	jb  	Ltest$2
 	ret 
